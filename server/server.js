315881f936640250b112bc52eefbe1efb2f568e2
Philipp Meier <philipp.meier@codecentric.de>
2015-03-08 08:20:30 +0100

    fix code

--- server/server.js
+++ server/server.js
@@ -3,13 +3,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-io.on('disconnect', function(socket){
-  console.log('a user disconnected');
+ 	console.log('a user connected');
+  	socket.on('disconnect', function(socket){
+  		console.log('a user disconnected');
+	});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+ 	console.log('listening on *:3000');
 });
