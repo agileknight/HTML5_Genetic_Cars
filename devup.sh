74b10007110945c9f0787e6051a67d3ea1eafdd2
Philipp Meier <philipp.meier@codecentric.de>
2015-03-08 18:31:43 +0100

    fix and improve docker files

--- devup.sh
+++ devup.sh
@@ -2,5 +2,4 @@
 
 docker build -t pmeier/cars_v2_web -f Dockerfile.client .
 docker build -t pmeier/cars_v2 -f Dockerfile.server .
-docker-compose pull
-docker-compose up -d
+docker-compose -f docker-compose-dev.yml up -d
