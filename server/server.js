776955a793213986b874c39d051e19df7e150684
Philipp Meier <philipp.meier@codecentric.de>
2015-03-08 19:27:43 +0100

    first part of server

--- server/server.js
+++ server/server.js
@@ -6,14 +6,59 @@ function log(message, socket) {
 	console.log(socket.id + ': ' + message);
 }
 
+var gameStates = {
+	init: {
+		joinGame: function (game, socket, data) {
+			socket.emit('game joined', {money: 2000});
+		}
+	},
+	betting: {
+
+	},
+	simulation: {
+
+	},
+	scoreing: {
+
+	},
+	end: {
+
+	}
+}
+
+var games = {};
+
+function gameExists(gameId) {
+	return games[gameId] != null;
+}
+
+function createGame(gameId) {
+	games[gameId] = {
+		players: {},
+		curState: gameStates.init
+	};
+}
+
+function joinGame(gameId, socket, data) {
+	var game = games[gameId];
+	if (game.curState.joinGame) {
+		game.curState.joinGame(game, socket, data);
+	}
+}
+
 io.on('connection', function(socket){
  	log('client connected', socket);
   	socket.on('disconnect', function(){
   		log('client disconnected', socket);
 	});
 	socket.on('join game', function(data) {
-		log('game joined', socket);
-		socket.emit('game joined', {money: 2000});
+		log('receive join game with data ' + JSON.stringify(data), socket);
+
+		if (!gameExists(data.gameId)) {
+			createGame(data.gameId);
+		}
+
+		joinGame(data.gameId, socket, data);
 	});
 });
 
